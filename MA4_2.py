454622ba170cbe97040f8f4c6df035844a36ff10
nrr138 <nrr138@gmail.com>
2021-10-16 15:29:46 +0200

    Updated Code

--- MA4_2.py
+++ MA4_2.py
@@ -1,12 +1,54 @@
 #!/usr/bin/env python3
 
 from integer import Integer
+import matplotlib.pyplot as plt
+from time import perf_counter as perf
 
 def main():
-	f = Integer(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+    
+    timescpp = []
+    timespy = []
+    values = range(30, 45)
+	
+	for n in values:
+		f = Integer(n)
+  
+		print("Timing for C++ where n =", n)
+		start = perf()
+		print(f.fib())
+		end = perf()
+		t = end - start
+		print(round("Time: ", t), 3)
+  		timescpp.append(t)
+
+		print("Timing for Python where n =", n)
+		start = perf()
+		print(fib_pure_py(n))
+		end = perf()
+		t = end - start
+		print(round("Time: ", t), 3)
+  		timespy.append(t)
+ 
+	print("Timing for C++ where n = 47")
+	f = Integer(47)
+	start = perf()
+	print(f.fib())
+	end = perf()
+	t = end - start
+	print(round("Time: ", t), 3)
+ 
+	plt.plot(values, timescpp, 'r.', values, timespy, 'b.')
+	plt.title("Computing time for fib(n)")
+	plt.xlabel("n")
+	plt.ylabel("Seconds")
+ 	plt.legend('C++', 'Python', loc="upper left")
+	plt.savefig("FibCompute.png")
 
 if __name__ == '__main__':
 	main()
+ 
+def fib_pure_py(n):
+    if n <= 1:
+        return n
+    else:
+        return(fib_pure_py(n-1) + fib_pure_py(n-2))
--- integer.cpp
+++ integer.cpp
@@ -6,7 +6,9 @@ class Integer{
 		Integer(int);
 		int get();
 		void set(int);
+		int fib();
 	private:
+		int _fib(int);
 		int val;
 	};
  
@@ -22,11 +24,28 @@ void Integer::set(int n){
 	val = n;
 	}
 
+int Integer::fib(){
+	int n;
+	n = val;
+	return _fib(n);
+}
+
+int Integer::_fib(int n){
+    if (n <= 1)
+	{
+		return n;
+	}
+	else
+	{
+		return _fib(n-1) + _fib(n-2);
+	}
+}
 
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
+	int Integer_fib(Integer* integer) {return integer->fib();}
 	void Integer_delete(Integer* integer){
 		if (integer){
 			delete integer;
